13dfdf8b0bb5cc58397e6bcab89e0523b9a6ad1e
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-05-03 13:25:56 +0200

    Changed History Mode to Hash Mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebBash, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 import AES_Encrypt from '../views/AES-Encrypt.vue'
 import AES_Decrypt from '../views/AES-Decrypt.vue'
@@ -46,7 +46,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHash(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
